0944e0686e539dfcaf0c4b278c34cd8f3ec68b01
Ujjwal Jha <128184072+ujjwaljha03@users.noreply.github.com>
2024-10-11 23:00:39 +0530

    Update axiosConfig.js

--- movie-gold-v1/src/api/axiosConfig.js
+++ movie-gold-v1/src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({                           // give http://localhost:8080 for local run. 
-    baseURL:'http://localhost:8080',
+    baseURL:'movies-reviews-production.up.railway.app',
     headers: {"ngrok-skip-browser-warning": "true"}
 });
